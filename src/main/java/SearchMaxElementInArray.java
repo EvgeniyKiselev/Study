b5b03a441cb8a795c38387dfa6b918611cc74a4a
EKiselev <mr.kiseleff@list.ru>
2020-01-20 09:34:10 +0300

    Рабочая версия SearchMaxElementInArray

--- src/main/java/SearchMaxElementInArray.java
+++ src/main/java/SearchMaxElementInArray.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class SearchMaxElementInArray {
 
     public static void main(String[] args) {
-        System.out.println("Результат операции: " + getMaxElement(getArray()));
+        System.out.println("Максимальный элемент в массиве: " + getMaxElement(getArray()));
     }
 
     public static int getArrayLength() {
         Scanner scanInputArrayLength = new Scanner(System.in);
         System.out.println("Задайте размер массива: ");
         int arrayLength = scanInputArrayLength.nextInt();
-        //scanInputArrayLength.close();
         return arrayLength;
     }
 
@@ -19,21 +18,17 @@ public class SearchMaxElementInArray {
         int arrayLength = getArrayLength();
         String[] newArray = new String[arrayLength];
         System.out.println("Введите слова для заполнения массива: ");
-        for (int i = 0; i < newArray.length - 1; i++) {
+        for (int i = 0; i < newArray.length; i++) {
             newArray[i] = scanInputElements.nextLine();
         }
         return newArray;
     }
 
     private static String getMaxElement(String[] newArray) {
-        String maxArrayElement = "";
-        int i = 0;
-        while (i == newArray.length) {
-            for (i = 0; i < newArray.length; i++) {
-                if (newArray[i].length() < newArray[i + 1].length()) {
-                    maxArrayElement = newArray[i];
-                }
-                maxArrayElement = newArray[i];
+        String maxArrayElement = newArray[0];
+        for (String s : newArray) {
+            if (maxArrayElement.length() < s.length()) {
+                maxArrayElement = s;
             }
         }
         return maxArrayElement;
