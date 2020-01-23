b7f26a04b1562af082c9ff49c376b441d2bb19b6
EKiselev <mr.kiseleff@list.ru>
2020-01-23 14:26:33 +0300

    TextToCollections

---
+++ src/main/java/TextToCollections.java
@@ -0,0 +1,44 @@
+import javax.management.ObjectName;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class TextToCollections {
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader read = new BufferedReader(new FileReader("C://Users//EKiselev//IdeaProjects//Study//src//main//java//textToWrite.txt"));
+        String line;
+        while ((line = read.readLine()) != null) {
+            list.addAll(Arrays.asList(line.split(" ")));
+            }
+        read.close();
+
+        System.out.println("Исходный массив:\n" + list);
+        Collections.sort(list);
+        System.out.println("Массив, отсортированный по алфавиту:\n" + list + "\n" + "Повторяющиеся слова: ");
+
+        int maxCount = 0;
+        String maxKey = "";
+        Map<String, Integer> map = countWords(list);
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+            if (pair.getValue() > maxCount) {
+                maxCount = pair.getValue();
+                maxKey = pair.getKey();
+            }
+        }
+
+        System.out.println("Слово с максимальным количеством повторений:\n" + "\"" + maxKey + "\"" + " " + maxCount);
+    }
+
+    public static Map<String, Integer> countWords(ArrayList<String> list) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        for (String s : list) {
+            result.put(s, Collections.frequency(list, s));
+        }
+        return result;
+    }
+}
